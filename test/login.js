bd7a0a3b1e949693f4babaa70cecef943caa7eae
John Price <jp_git@gcfl.net>
2021-04-13 15:44:46 -0500

    Fix demo test

--- test/login.js
+++ test/login.js
@@ -2,13 +2,14 @@ const { expect } = require("chai");
 const fs = require('fs');
 
 describe('Auth functions', () => {
-	before(done => {
-		fs.copyFileSync('config.js', 'config-prod.js', (err) => {
-			if (err) {
-				throw err;
-			}
-		})
-	});
+	// before(done => {
+	// 	fs.copyFileSync('config.js', 'config-prod.js', (err) => {
+	// 		if (err) {
+	// 			throw err;
+	// 		}
+	// 		done();
+	// 	})
+	// });
 
 	it('user signup', () => {
 		// create new database
